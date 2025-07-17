fcc7f17f2152afcb8b27b1ac6b7dd82efecb3bc1
Krisha Mewada <krishamewada06@gmail.com>
2025-07-17 15:40:02 +0530

    Add files via upload

---
+++ CompilerDesignBasics.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+//keywords
+
+char keyword [][10] = {"int","float","if","else","char"};
+
+//function to check : if word is a keyword
+
+int isKeyword(char word[]){
+    for(int i=0;i<5;i++)
+    {
+        if(strcmp(word,keyword[i])==0)
+        {
+            return 1; //  it is a keyword 
+        }
+    }
+    return 0; //  it is not 
+}
+
+// funtion to check if a character is operator
+
+int isOperator (char ch){
+    if(ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '=' || ch == '>')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int main(){
+    FILE *ptr;
+    char ch;
+    char word[50];
+    int i = 0;
+
+    // inputs from file 
+
+    ptr = fopen("input.txt","r");
+    if(ptr == NULL){
+        printf("ERROR\n");
+        return 1;
+    }
+
+    printf("Lexical Analysis\n");
+
+    while((ch = fgetc(ptr)) != EOF){
+    
+        if(isalnum(ch)){
+            word[i++] = ch;
+        }
+        else{
+            word[i] = '\0';
+            i = 0;
+
+            //
+
+            if(strlen(word)>0){
+                // int isKey = 0;
+                
+                // for(int j=0;j<5;j++)
+                // {
+                //     if(strcmp(word,keyword[j]) == 0)
+                //     {
+                //         isKey = 1;
+                //         break;
+                //     }
+                // }
+            
+             if (isKeyword(word))
+             {
+                printf("Keyword: %s\n", word);
+             }
+             else {
+                printf("Identifier: %s\n", word);
+             }
+        }
+         if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '=' || ch == '>') 
+        {
+            printf("Operator: %c\n", ch);
+        }
+        }
+    }
+    fclose(ptr);
+    return 0;
+}
+    
+
